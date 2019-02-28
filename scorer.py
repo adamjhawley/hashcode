88d67f35119247f0efeadbec8e7a0b11432bfd3f
SM Langridge <sml540@cse069pc-17.cs.york.ac.uk>
2019-02-28 19:19:01 +0000

    Wrote code to deal with vertical slides, untested

--- scorer.py
+++ scorer.py
@@ -2,14 +2,13 @@
 def calc_score(slides):
     scores = []
     for i in range(1, len(slides)):
-        scores.append(calc_transition_score(slides[i - 1], slides[i]))
+        scores.append(calc_transition_score(get_tags(slides[i - 1]),
+                                            get_tags(slides[i])))
     return(scores)
 
 
 # Takes two slides and returns the score between them
-def calc_transition_score(s1, s2):
-    left = s1.tags
-    right = s2.tags
+def calc_transition_score(left, right):
     common = 0
     only_l = 0
     only_r = 0
@@ -23,3 +22,10 @@ def calc_transition_score(s1, s2):
     only_r = len(right)
 
     return min(common, only_l, only_r)
+
+# Gets the tags from a slide, vertical or horizontal
+def get_tags(slide):
+    if type(slide) is tuple:
+        return slide[0].tags.extend(slide[1].tags)
+    else:
+        return slide.tags
